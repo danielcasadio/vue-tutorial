e91bd0ef8c6f018129282a9cc440374fafea7167
daniel <danielcasadio@id.uff.br>
2020-07-16 18:10:52 -0300

    Validaçoes

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,7 @@
 class Favorite < ApplicationRecord
   belongs_to :favoritable, polymorphic: true
   belongs_to :user
+
+  validates :user_id, uniqueness: {scope: [:favoritable_type, :favoritable_id],
+  message: 'Can only favorite one time per movie/episode'}
 end
